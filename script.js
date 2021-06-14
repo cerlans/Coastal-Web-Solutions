57e86e1542891b93c5c71ea9e0f48f98a4271d3f
cerlans <kgomez222@hotmail.com>
2021-06-14 12:36:08 -0400

    5 files modified

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
 (function () {
- const burgerNav = document.getElementById('burger')
- const closeNav = document.getElementById('closeNav');
-
-burgerNav.addEventListener('click',()=>{
-  const hiddenMenu = document.getElementById('hiddenMenu');
-  hiddenMenu.style.display = 'block';
-})
-
-
+  let burger = document.getElementById('burgerDirect');
+  let closeMark = document.getElementById('closeMark');
+  burger.addEventListener('click',()=>{
+    let hiddenMenu = document.getElementById('hiddenMenu');
+    hiddenMenu.style.display = 'block';
+  })
+  closeMark.addEventListener('click',()=>{
+       let hiddenMenu = document.getElementById('hiddenMenu');
+    hiddenMenu.style.display = 'none';
+  })
 })();
